bf40c749a76880f70da5a075059277e3e65b96ff
F-Ting <chen.daniel2099@gmail.com>
2023-10-24 21:05:22 -0400

    Using pointer-events property to change parent background from child node hover event

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,17 +5,12 @@ import BubbleContainer from "./bubbles";
 const Home = () => {
   return (
     <section className="home" id="home">
-      <div className="intro">
-        <h2 className="home_section_title">Hi! This is Wanqing!</h2>
-      </div>
+      <h2 className="home_section_title">Hi! This is Wanqing!</h2>
       <BubbleContainer />
-      <div className="background_Img" id="bgImage">
-        <div className="home_button">
-          <a href="#about" className="home_button_title">
-            Know More About Me!
-          </a>
-        </div>
-        <div className="home_background"></div>
+      <div className="home_button">
+        <a href="#about" className="home_button_title">
+          Know More About Me!
+        </a>
       </div>
     </section>
   );
