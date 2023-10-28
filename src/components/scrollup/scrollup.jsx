43747fc4f78cf7805c65492d9fbb7024e5798a94
F-Ting <chen.daniel2099@gmail.com>
2023-10-27 20:09:01 -0400

    updating scroll up jsx

--- src/components/scrollup/scrollup.jsx
+++ src/components/scrollup/scrollup.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import './scrollup.css';
 
-const ScrollUp =() => {
-    window.addEventListener("scroll", function(){
+const ScrollUp = () => {
+    window.addEventListener("scroll", function () {
         const scrollup = document.querySelector(".scrollup");
-        if (this.scrollY >= 560 ) scrollup.classList.add("show-scroll");
+        if (this.scrollY >= 560) scrollup.classList.add("show-scroll");
         else scrollup.classList.remove("show-scroll")
     })
     return (
-        <a href="" className="scrollup">
+        <a href="#home" className="scrollup">
             <i className="uil uil-arrow-up scrollup_icon"></i>
         </a>
     )
